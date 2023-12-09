ce22651cfcfa9a9e3b91c922e6a5cdd754012da7
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2023-12-09 08:48:08 +0400

    Day four (2023/12/9)

---
+++ script.js
@@ -0,0 +1,18 @@
+//Used for the voices
+const sounds = [
+  "fakhri",
+  "habib",
+  "karim1",
+  "karim2",
+  "mahmoud1",
+  "mahmoud2",
+  "mahmoud3",
+  "saif1",
+  "saif2",
+  "saif3",
+];
+
+//JS training
+let x = 10;
+x = 11;
+console.log(x);
