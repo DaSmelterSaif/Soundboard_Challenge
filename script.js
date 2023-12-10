6be7d12f4c795b5dc5ad6351d99157fb9883d930
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2023-12-10 16:56:54 +0400

    Day five (2023/12/10)

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//Used for the voices
-const sounds = [
-  "fakhri",
-  "habib",
-  "karim1",
-  "karim2",
-  "mahmoud1",
-  "mahmoud2",
-  "mahmoud3",
-  "saif1",
-  "saif2",
-  "saif3",
-];
+const fakhri = new Audio("voices/fakhri-siu.mp3");
+const habib = new Audio("voices/habib-bro.mp3");
+const karim1 = new Audio("voices/karim-altak-3l-radian.mp3");
+const karim2 = new Audio("voices/karim-salam-alaikum.mp3");
+const mahmoud1 = new Audio("voices/mahmoud-bekol-sohoola.mp3");
+const mahmoud2 = new Audio("voices/mahmoud-lets-go.mp3");
+const mahmoud3 = new Audio("voices/mahmoud-okay.mp3");
+const saif1 = new Audio("voices/saif-@@@.mp3");
+const saif2 = new Audio("voices/saif-aloo-glitch.mp3");
+const saif3 = new Audio("voices/saif-shabab.mp3");
 
-//JS training
-let x = 10;
-x = 11;
-console.log(x);
+function audioP(p) {
+  p.play();
+}
